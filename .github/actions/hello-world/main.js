774fcfc5d8a666914729515c45911197e80e2145
Benjamin Lee <ben@2nproductions.com>
2021-07-11 18:13:21 -0700

    Add in call

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -5,6 +5,10 @@ const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 
+const client = github.getOctokit(
+	core.getInput('input', {required: true});
+);
+
 console.log(`Hello ${firstGreeting}`);
 console.log(`Hello ${secondGreeting}`);
 if (thirdGreeting) {
@@ -13,6 +17,18 @@ if (thirdGreeting) {
 
 if (github.context.eventName === 'push') {
     console.log(`${github.context.payload}`);
-    console.log(`${github.context.payload.base}`);
-	console.log(`${github.context.payload.head}`);
+    console.log(`${github.context.payload.before}`);
+	console.log(`${github.context.payload.after}`);
+} else if (github.context.eventName === 'push_request') {
+    console.log(`${github.context.payload}`);
+    console.log(`${github.context.payload.pull_request.base}`);
+	console.log(`${github.context.payload.pull_request.head}`);
+
+	const response = await client.rest.repos.compareCommitsWithBasehead( {
+		owner: github.context.repo.owner,
+		repo: github.context.repo.repo,
+		basehead: `${github.context.payload.pull_request.base.sha}..${github.context.payload.pull_request.head.sha}`
+	});
+
+	console.log(`${response}`);
 }
