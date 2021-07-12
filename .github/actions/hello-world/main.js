cb524390d970681343ae7ba5edf666bf4994a174
Benjamin Lee <ben@2nproductions.com>
2021-07-11 21:41:25 -0700

    output keys

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -31,7 +31,7 @@ async function run() {
 
 		console.log(`${base}`);
 		console.log(`${head}`);
-		core.info(`${github}`);
+		console.log(Object.keys(github.context.payload.repo));
 		const response = await client.rest.repos.compareCommitsWithBasehead( {
 			//base,
 			//head,
